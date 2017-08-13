f2841a5a09fc5293fcb286b934ac4b1868451b3f
Daniel Ideriba <danielideriba@Daniels-Mac-mini-2.local>
2017-08-12 23:20:39 -0300

    little port fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 80,
   mongoose = require('mongoose'),
     Task = require('./api/models/dekretoModel'),
     bodyParser = require('body-parser');
