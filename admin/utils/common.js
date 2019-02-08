6c903f244e997a22c3f623aaf2d37a8bb74b6b42
Daniel Ideriba <danielideriba@Daniels-Mac-mini-2.local>
2019-02-08 00:43:17 -0200

    bugfix infinity

--- admin/utils/common.js
+++ admin/utils/common.js
@@ -1,3 +1,4 @@
+'use strict';
 module.exports = {
   //Access Control
   ensureAuthenticated: function (req, res, next){
--- api/routes/types.js
+++ api/routes/types.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require('express');
 const router = express.Router();
 const common = require('../../admin/utils/common');
