11db3d51473463cb668730f07823ee6b064d99b4
Daniel Ideriba <danielideriba@Daniels-Mac-mini-2.local>
2019-02-20 02:19:50 -0300

    api events

--- api/routes/events.js
+++ api/routes/events.js
@@ -6,4 +6,17 @@ const common = require('../../admin/utils/common');
 //models
 let Events = require('../../admin/models/events');
 
+//List
+router.get('/list', function(req, res){
+  var query = {__v: 0};
+
+  Events.find({}, query, function(err, events){
+    if(err){
+      console.log(err);
+    } else {
+      res.json({events: events});
+    }
+  });
+});
+
 module.exports = router;
