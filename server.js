eac5dbe652f105099e2e90a2cc748eddc4a87ca5
Daniel Ideriba <danielideriba@Daniels-MacBook-Pro.local>
2019-04-14 23:25:21 -0300

    auth retirado

--- server.js
+++ server.js
@@ -145,30 +145,30 @@ router.post(authPath, function(req, res) {
   });
 });
 
-router.use(function(req, res, next) {
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  // decode token
-  if (token) {
-    // verifies secret and checks exp
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-      if (err) {
-        return res.json({ success: false, message: 'Falha na autenticação.' });
-      } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    // if there is no token
-    // return an error
-    return res.status(403).send({
-        success: false,
-        message: 'Token não fornecido.'
-    });
-  }
-});
+// router.use(function(req, res, next) {
+//   // check header or url parameters or post parameters for token
+//   var token = req.body.token || req.query.token || req.headers['x-access-token'];
+//   // decode token
+//   if (token) {
+//     // verifies secret and checks exp
+//     jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+//       if (err) {
+//         return res.json({ success: false, message: 'Falha na autenticação.' });
+//       } else {
+//         // if everything is good, save to request for use in other routes
+//         req.decoded = decoded;
+//         next();
+//       }
+//     });
+//   } else {
+//     // if there is no token
+//     // return an error
+//     return res.status(403).send({
+//         success: false,
+//         message: 'Token não fornecido.'
+//     });
+//   }
+// });
 
 //Routes admin
 let registerUsers = require(__dirname+adminPath+'/routes/users');
